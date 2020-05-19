7e1bfdfd6e38f26100da86b76da110105ec88d74
GrupoTic <dev.grupotic@gmail.com>
2020-05-18 19:34:51 -0500

    new change

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/akp', (req, res) => res.send('Abraham KP'))
+app.get('/test', (req, res) => res.send('Grupo Tic'))
 
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
